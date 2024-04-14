9a662259582c09e2139946a950159dfbb146e624
Natalia Bandurova <natacsp@mail.ru>
2024-04-14 21:44:32 +0300

    feat: add solution for queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -147,8 +147,16 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce(
+    (acc, promise) =>
+      acc.then((result) =>
+        promise.then(
+          (resolvedValue) => result.toString() + resolvedValue.toString()
+        )
+      ),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
